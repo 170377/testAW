23ead68525985957782a05cf7ace7732e56d8e75
Luis <170377@iessierradeguara.com>
2020-11-04 17:16:10 +0100

    Fin clase

---
+++ aw-master/web-jvs1/app.js
@@ -0,0 +1,71 @@
+// alert("Texto");
+
+// Tipos de datos //
+// /* Tipo de cadena de texto o string */
+// alert("This is Cadena de texto")
+// alert('This is cadena de texto too')
+// alert("I'm a student")
+
+//Tipo número entero //
+// Se pueden usar operadores aritmeticos //
+// alert(2 + 2 + 2 - 2 / 2 * 5);
+// alert("2" + 4);
+
+//Tipo número decimal //
+// Se pueden usar operadores aritmeticos //
+// alert(2 + 2 + 2.4 - 2.5 / 2 * 5);
+// alert("2" + 4.5);
+
+// Tipo booleano //
+// true false //
+// Operaciones lógicas &&  //
+// alert(true && true);
+// alert(false || true);
+
+// Definir variables //
+// let mensaje = "Texto";
+// let nombre = "Console";
+// let numero;
+// numero = 3 + 2;
+// numero++;
+// alert(numero);
+// numero--;
+// alert(numero);
+
+// alert(mensaje);
+// alert("Hola" + nombre);
+// console.log(mensaje);
+
+
+// let variable1 = 3;
+// let variable2 = 6;
+// let variable3 = 4;
+// alert(variable1 + variable2 + variable3);
+
+// let Variable1 = 76;
+// let Variable2 = 23;
+// let resultado = (Variable1 * Variable2);
+// alert(resultado);
+
+// Document.write
+
+// document.write( < h1 > "Texto" < /h1>);
+// alert("Mira el h1");
+// let elemento = document.getElementById("encabezado");
+// elemento.innerHTML = "No texto"
+// alert("Mira el h1");
+// elemento.innerHTML = "NooN texto"
+// alert("Mira el h1");
+// elemento.innerHTML = "NoO0 texto"
+// alert("Mira el h1");
+// elemento.innerHTML = "NoOOOOOOOOOOO texto"
+
+// Confirm
+
+let resultado;
+resultado = confirm("¿Quieres un saludo we?");
+
+if (resultado == true) {
+    alert("HOLAAAA C:");
+}
+alert("fin del programa");
