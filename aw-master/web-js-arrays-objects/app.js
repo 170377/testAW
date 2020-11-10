f9d6e7d890d5c98696f3c94b720c77c1ea8fb91d
Luis <170377@iessierradeguara.com>
2020-11-10 18:58:21 +0100

    Fin de la clase

---
+++ aw-master/web-js-arrays-objects/app.js
@@ -0,0 +1,17 @@
+let alumnoArray = ["Lorem", "Ipsum", 54];
+// console.log(alumnoArray[0]);
+// console.error(alumnoArray[1]);
+
+// alert(alumnoArray[2]);
+
+// console.log(alumno.length);
+
+let alumnoObjeto = {
+    "nombre": "Lorem",
+    "apellido": "Ipsum",
+    "edad": 54
+}
+
+console.log(alumnoObjeto.nombre);
+console.error(alumnoObjeto.apellido);
+alert(alumnoObjeto.edad);
