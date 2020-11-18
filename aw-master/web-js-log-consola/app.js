78a745e133c2fd41d6c6e66f10214419be4a05e2
Luis <170377@iessierradeguara.com>
2020-11-18 15:50:25 +0100

    Repositorio profe

--- aw-master/web-js-log-consola/app.js
+++ aw-master/web-js-log-consola/app.js
@@ -1,7 +1,8 @@
-console.log("Welcome to console");
-console.error("Consola de error")
+console.log("Hola mundo");
+console.error("Esto es la consola de error");
 
+/*Esto es una declaración de una constante*/
 const saludo = "hola";
 
-// Esta línea de error, porque a una constante no se le puede cambiar el valor
-saludo = "adiós";
+// Esta línea da error, porque a una constante no se le puede cambiar el valor
+saludo = "adios";
