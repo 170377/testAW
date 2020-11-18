74ebe82d0303f68605e28f880244477f870fbb49
Luis <170377@iessierradeguara.com>
2020-11-18 17:05:55 +0100

    Fin de clase

--- aw-master/web25-js-listaTareas/app.js
+++ aw-master/web25-js-listaTareas/app.js
@@ -19,3 +19,27 @@ function anyadirElemento(textoTarea) {
     // del parámetro textoTarea
     listaTareas.innerHTML += "<li>" + textoTarea + "</li>";
 }
+
+// Esto es equivalente
+
+function BorrarTarea() {
+    //Usar prompt para pedir al usuario un número de tarea.
+    let numTarea = prompt("Selecciona el número de tarea que deseas eliminar tarea");
+    //Comprobar que el número de tarea es válido
+    //capturo la lista de tareas.
+    let lTareas = document.getElementById("listaTareas")
+    if (numTarea >= 1 0 && numTarea <= lTareas.children - length) {
+        //Borrar de la lista de tareas el elemento número
+        //n-1, donde n es el valor que me ha pasado el usuario
+        numTarea = numTarea - 1; //numTarea--; numTarea-=1;
+
+
+        //capturo el elemento que quiero borrar
+        let eTareaBorrar = lTareas.children[numTarea];
+        //borro ese elemento de la lista
+        lTareas.removeChild(lTareas);
+
+    } else() {
+        alert("Ese número no existe");
+    }
+}
